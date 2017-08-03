c7eab91d4b124a97ff27481a8fa850a52a2c0910
docdvz@gmail.com <gbpltw123>
2017-08-03 16:29:44 +0300

    it's alive ignite client

--- src/main/java/ru/iitdgroup/app/IgniteStarter.java
+++ src/main/java/ru/iitdgroup/app/IgniteStarter.java
@@ -5,18 +5,22 @@ import org.apache.ignite.Ignition;
 import java.io.File;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.Properties;
 
 public class IgniteStarter {
 
     public void init() {
-        URL url;
-        try {
-            url = new URL("file:/resources/ignite-config.xml");
-            System.out.println(Paths.get(url.toURI()));
-        } catch (Exception e){
-            e.printStackTrace();
-        }
 
-        Ignition.start("/config/ignite-config.xml");
+//        Properties p = System.getProperties();
+//        Enumeration keys = p.keys();
+//        while (keys.hasMoreElements()) {
+//            String key = (String)keys.nextElement();
+//            String value = (String)p.get(key);
+//            System.out.println(key + ": " + value);
+//        }
+        String path = System.getProperty("catalina.home") + "\\conf\\ignite-config.xml";
+        Ignition.start(path);
+//        Ignition.start();
     }
 }
